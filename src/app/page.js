a26f209fcde31b9c2f56e9f4c2952bd1d5b4ece2
DavidSitompul <dpaulsitompul@gmail.com>
2024-02-28 21:29:32 +0700

    updating name website and fixing layout and cv

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,8 @@ import Contact from "@/components/contact"
 
 export default function Home() {
   return (
-    <main className="lg:ml-10">
-      <div className="h-auto w-full lg:h-[500px] mt-16">
+    <main className="w-full">
+      <div className="h-auto w-[90%] m-auto lg:h-[500px] mt-16 ">
         <div className="w-full lg:w-[50%] h-auto">
           <h1 className="text-[20px] lg:text-[33px] text-[#464660]">
             Hi There, I’m David Paul Sitompul
@@ -26,7 +26,7 @@ export default function Home() {
             <p className="indent-3"> Bachelor of Computer Science, University of North Sumatra. Still learning about programming</p>
             <p className="indent-3 mt-2 italic">{`"There are no failed dreams only delayed dreams. If you feel like youve failed to achieve your dream dont worry another dream can be created"`}</p>
           </div>
-          <a href="/cv/Transkip Nilai_terbaru.pdf" download={true}>
+          <a href="/cv/David Paul Sitompul-resume.pdf" download={true}>
             <button className="bg-[#368B85] px-5 py-2 mt-6 lg:mt-12 rounded-lg shadow-md text-white font-semibold">Download My CV</button>
           </a>
         </div>
