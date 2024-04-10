e0c7c41c1a510abf00f92c379863b7d00f24637f
DavidSitompul <dpaulsitompul@gmail.com>
2024-04-10 12:57:12 +0700

    add new file project and image

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -9,7 +9,7 @@ const Project = () => {
       images: "/img/marketPlace.png",
       link: "https://marketplaceulos.vercel.app",
       judul: "Final Project",
-      ket: "marketplace platform for ulos products and is expected to develop as a shopping platform for other local products.",
+      ket: "Build with nextJs 13,Firebase, Flowbite Components,react icon and SwiperJs.",
     },
     {
       images: "/img/GMI.png",
@@ -22,6 +22,12 @@ const Project = () => {
       link: "https://github.com/DavidSitompul/LandingpageRental",
       judul: "Rentals Car",
       ket: "landing page application for car rental",
+    },
+    {
+      images: "/img/Anime.png",
+      link: "https://vidsanime.vercel.app/",
+      judul: "VIDSANIME",
+      ket: "Website Anime build with Jikan free Api, Chakra Ui, React-Icons, React-Youtube, And SwiperJs",
     },
   ]
 
@@ -72,7 +78,7 @@ const Project = () => {
                   <h5 className="text-2xl font-bold tracking-tight ">{img.judul}</h5>
                   <p className="font-normal">{img.ket}</p>
                   <Button className="mt-5 w-40" href={img.link}>
-                    Read More
+                    Code or Demo
                     <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                       <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
                     </svg>
